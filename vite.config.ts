1e5564f81c91aba18a84f25542334a326fc54325
Fye <hereisnirvana@hotmail.com>
2023-12-30 16:49:34 -0600

    Paths added to tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     outDir: 'docs',
   },
   base: '/vite-deploy-test/',
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, './src')
+    }
+  }
 })
