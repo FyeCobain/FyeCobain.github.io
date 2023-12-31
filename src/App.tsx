2764e07c9987df20f2bcc63c81625ab231a72988
Fye <hereisnirvana@hotmail.com>
2023-12-30 23:38:25 -0600

    'About me', basic info added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Nav from './components/Nav'
 import Header from './components/Header'
+import AboutMe from './components/aboutMe/AboutMe'
 
 function App() {
   return (
     <>
       <Nav />
       <Header />
+      <main>
+        <AboutMe />
+      </main>
     </>
   )
 }
