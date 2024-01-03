918747a991f5abcf9755563e3c3cf7f0ecb8f487
Fye <hereisnirvana@hotmail.com>
2024-01-03 15:34:16 -0600

    Skills added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Nav from './components/Nav'
 import Header from './components/header/Header'
 import AboutMe from './components/aboutMe/AboutMe'
-
 function App() {
   return (
     <>
       <Nav />
       <Header />
-      <main>
-        <AboutMe />
-      </main>
+      <AboutMe />
     </>
   )
 }
