646c6d8743bc9851744cb6ab490de89f5d5883b0
Fye <hereisnirvana@hotmail.com>
2024-01-03 23:31:42 -0600

    Image overlay working

--- src/components/header/Photo.tsx
+++ src/components/header/Photo.tsx
@@ -1,14 +1,18 @@
 import { useTranslation } from 'react-i18next'
 import photo from '/img/endlc.jpg'
+import { useContext } from 'react'
+import { type ImageContextValueInterface } from '@app/types-interfaces'
+import { ImageContext } from '@app/contexts'
 
 export default function Photo() {
   const { t } = useTranslation()
+  const imageState: ImageContextValueInterface = useContext(ImageContext)
 
   return (
     <div className="header__photo content-center">
 
       <div className="header__photo-container">
-        <img width="200" height="200" src={ photo } alt={ t('alts.photo') } />
+        <img width="200" height="200" src={ photo } alt={ t('alts.photo') } onClick={ () => imageState.setImage(photo) } />
         <div className="header__photo-container--overlay"></div>
       </div>
 
