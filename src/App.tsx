aeb46c7ba955f095f9c87ebb80532a7073a1a08e
Fye <hereisnirvana@hotmail.com>
2024-01-03 22:19:17 -0600

    Image overlay component and context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import ImageOverlay from './components/ImageOverlay'
 import Nav from './components/Nav'
 import Header from './components/header/Header'
 import AboutMe from './components/aboutMe/AboutMe'
+import { ImageContext } from './contexts'
+
 function App() {
   return (
-    <>
+    <ImageContext.Provider value={ null }>
+      <ImageOverlay />
       <Nav />
       <Header />
       <AboutMe />
-    </>
+    </ImageContext.Provider>
   )
 }
 
