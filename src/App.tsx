042f2fd8afab6a0114e3e546559be1e860493330
Fye <hereisnirvana@hotmail.com>
2024-01-04 15:00:42 -0600

    Minor styles improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { useState } from 'react'
 import ImageOverlay from './components/ImageOverlay'
 import Nav from './components/Nav'
 import Header from './components/header/Header'
 import AboutMe from './components/aboutMe/AboutMe'
 import { ImageContext } from './contexts'
-import { useState } from 'react'
 import { type stringNullable, type ImageContextValueInterface } from './types-interfaces'
 
 function App() {
+  // Image state context for the image oberlay
   const [ image, setImage ] = useState<stringNullable>(null)
   const imageState: ImageContextValueInterface = {
     image,
