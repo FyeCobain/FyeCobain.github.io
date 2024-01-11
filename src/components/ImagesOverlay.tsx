2243b700ff1db2dbdf0ec421412c17c7c8911e8b
Fye <hereisnirvana@hotmail.com>
2024-01-10 19:33:16 -0600

    Images slider working

--- src/components/ImagesOverlay.tsx
+++ src/components/ImagesOverlay.tsx
@@ -1,26 +1,14 @@
-import { type MouseEvent, useContext, useEffect, useRef, type MutableRefObject } from 'react'
+import { type MouseEvent, useContext, useEffect, useRef, type MutableRefObject, useCallback } from 'react'
 import { type DivNullable, type ImagesContextValueInterface } from '@app/types-interfaces'
-import { ImagesContext } from '@app/contexts'
 import { IoMdCloseCircleOutline } from 'react-icons/io'
+import { GrPrevious } from 'react-icons/gr'
+import { ImagesContext } from '@app/contexts'
 
-// Removes the images from the slider
-function removeImages(imagesState: ImagesContextValueInterface, targetElement: Element | null) {
-  if (targetElement === null || targetElement.tagName !== 'IMG')
+// Removes all the images from the slider
+function removeImages(imagesState: ImagesContextValueInterface, targetElement: Element | null = null) {
+  if (targetElement === null || targetElement.classList.contains('images-overlay__slider') || targetElement.classList.contains('images-overlay__close-button')) {
     imagesState.setImages([])
-}
-
-// Handles the key pressing
-function onKeyDown(event: KeyboardEvent, imagesState: ImagesContextValueInterface, sliderDiv: DivNullable) {
-  switch (event.key) {
-    case 'Escape':
-      removeImages(imagesState, null)
-      break
-    case 'ArrowLeft':
-      previousImg(sliderDiv)
-      break
-    case 'ArrowRight':
-      nextImg(sliderDiv)
-      break
+    imagesState.setCurrentImageIndex(0)
   }
 }
 
@@ -35,56 +23,103 @@ function getCurrentImgIndex(imgs: HTMLImageElement[]): number {
   return imgs.findIndex((img: HTMLImageElement) => !img.classList.contains('prev') && !img.classList.contains('next'))
 }
 
+// Checks if the 'previous' or the 'next' button must be disabled
+function checkButtons(imagesState: ImagesContextValueInterface, currentImageIndex: number) {
+  const prevButton: HTMLElement | null = document.querySelector('[class*="images-overlay__prev-button"]')
+  const nextButton: HTMLElement | null = document.querySelector('[class*="images-overlay__next-button"]')
+  if (prevButton === null || nextButton === null) return
+
+  if (currentImageIndex === 0)
+    prevButton.classList.add('display-none')
+  else
+    prevButton.classList.remove('display-none')
+
+  if (currentImageIndex === imagesState.images.length - 1)
+    nextButton.classList.add('display-none')
+  else
+    nextButton.classList.remove('display-none')
+}
+
 // Show previous image
-function previousImg(sliderDiv: DivNullable) {
+function previousImg(imagesState: ImagesContextValueInterface, sliderDiv: DivNullable) {
   const imgs: HTMLImageElement[] = getImgs(sliderDiv)
   if (imgs.length <= 1) return
-  const currentImgIndex = getCurrentImgIndex(imgs)
-  if (currentImgIndex === 0) return
+  const currentImageIndex = getCurrentImgIndex(imgs)
+  if (currentImageIndex === 0) return
 
-  imgs[currentImgIndex].classList.add('next')
-  imgs[currentImgIndex - 1].classList.remove('prev')
+  imgs[currentImageIndex].classList.add('next')
+  imgs[currentImageIndex - 1].classList.remove('prev')
+  imagesState.setCurrentImageIndex(currentImageIndex - 1)
+  checkButtons(imagesState, currentImageIndex - 1)
 }
 
 // show next image
-function nextImg(sliderDiv: DivNullable) {
+function nextImg(imagesState: ImagesContextValueInterface, sliderDiv: DivNullable) {
   const imgs: HTMLImageElement[] = getImgs(sliderDiv)
   if (imgs.length <= 1) return
-  const currentImgIndex = getCurrentImgIndex(imgs)
-  if (currentImgIndex === imgs.length - 1) return
+  const currentImageIndex = getCurrentImgIndex(imgs)
+  if (currentImageIndex === imgs.length - 1) return
 
-  imgs[currentImgIndex].classList.add('prev')
-  imgs[currentImgIndex + 1].classList.remove('next')
+  imgs[currentImageIndex].classList.add('prev')
+  imgs[currentImageIndex + 1].classList.remove('next')
+  imagesState.setCurrentImageIndex(currentImageIndex + 1)
+  checkButtons(imagesState, currentImageIndex + 1)
 }
 
 export default function ImagesOverlay() {
   const imagesState: ImagesContextValueInterface = useContext(ImagesContext)
-  const sliderRef: MutableRefObject<HTMLDivElement | null> = useRef(null)
-  const keyListenerAddedRef: MutableRefObject<boolean> = useRef(false)
+  const sliderRef: MutableRefObject<DivNullable> = useRef(null)
+  const listenerAddedRef: MutableRefObject<boolean> = useRef(false)
 
+  // Creating the keydown callback
+  const onKeyDownCallback = useCallback((event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'Escape':
+        removeImages(imagesState)
+        break
+      case 'ArrowUp':
+        previousImg(imagesState, sliderRef.current)
+        break
+      case 'ArrowLeft':
+        previousImg(imagesState, sliderRef.current)
+        break
+      case 'ArrowRight':
+        nextImg(imagesState, sliderRef.current)
+        break
+      case 'ArrowDown':
+        nextImg(imagesState, sliderRef.current)
+    }
+  }, [ imagesState.images ])
+
+  // Adding / removing the keydown callback
   useEffect(() => {
-    // Adding the keydown listener
-    if (!keyListenerAddedRef.current)
-      document.addEventListener('keydown', (event: KeyboardEvent) => onKeyDown(event, imagesState, sliderRef.current))
+    if (!listenerAddedRef.current && imagesState.images.length > 0)
+      document.addEventListener('keydown', onKeyDownCallback)
 
     return () => {
-      keyListenerAddedRef.current = true
+      if (imagesState.images.length > 0)
+        listenerAddedRef.current = true
+      document.removeEventListener('keydown', onKeyDownCallback, true)
     }
-  }, [])
+  }, [ onKeyDownCallback ])
 
   useEffect(() => {
+    // Adding the 'overflow-hidden' class to the body
     if (imagesState.images.length > 0)
       document.body.classList.add('overflow-hidden')
-    else
+    else {
       document.body.classList.remove('overflow-hidden')
+      return
+    }
 
-    if (sliderRef.current === null) return
+    // Checking if buttons must be enabled or disabled
+    checkButtons(imagesState, imagesState.currentImageIndex)
 
     // Adding the 'next' class to the images starting from the second one
-    sliderRef.current.querySelectorAll('img:not(:first-of-type)').forEach((img: Element) => {
+    sliderRef?.current?.querySelectorAll('img:not(:first-of-type)').forEach((img: Element) => {
       img.classList.add('next')
     })
-  }, [ imagesState ])
+  }, [ imagesState.images ])
 
   if (imagesState.images.length === 0)
     return <></>
@@ -100,8 +135,24 @@ export default function ImagesOverlay() {
       }
       </div>
 
-      <div className="images-overlay__close-button">
+      <div className="images-overlay__close-button" >
         <IoMdCloseCircleOutline className />
+      </div>
+
+      <div className={'images-overlay__current-info' + (imagesState.images.length <= 1 ? ' display-none' : '')} >
+        <p>{ `${ imagesState.currentImageIndex + 1 } / ${ imagesState.images.length }` }</p>
+      </div>
+
+      <div
+        className={'images-overlay__prev-button content-center' + (imagesState.images.length <= 1 ? ' display-none' : '')}
+        onClick={ () => previousImg(imagesState, sliderRef.current) } >
+        <GrPrevious />
+      </div>
+
+      <div
+        className={'images-overlay__next-button content-center' + (imagesState.images.length <= 1 ? ' display-none' : '')}
+        onClick={ () => nextImg(imagesState, sliderRef.current) } >
+        <GrPrevious />
       </div>
 
     </div>
