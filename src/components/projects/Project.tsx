7f20c7b85140bac616a51f832b4c1d2aad803f57
Fye <hereisnirvana@hotmail.com>
2024-01-15 09:52:40 -0600

    Slider, max-height option added

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -8,19 +8,26 @@ export default function Project({ project }: { project: ProjectInterface }) {
   const imagesState: ImagesContextValueInterface = useContext(ImagesContext)
 
   function setImages() {
+    imagesState.setMaxHeight(project.maxHeight)
     imagesState.setImages(project.slider_images)
   }
 
   return (
     <div className="project" >
       <div className="project__heading">
-        {
-        project.link === null
-          ? <p>{ project.title }</p>
-          : <><a target={ project.link.toLowerCase().trim().startsWith('http') ? '_blank' : '' } rel="noreferrer" href={ project.link }>{ project.title } { project.link.toLowerCase().trim().startsWith('http') ? <LiaExternalLinkSquareAltSolid /> : <IoMdDownload /> }</a></>
-        }
+      {
+      project.link === null
+        ? <p>{ project.title }</p>
+        : <a
+            rel="noreferrer"
+            target={ project.link.toLowerCase().trim().startsWith('http') ? '_blank' : '' }
+            href={ project.link }>{ project.title } { project.link.toLowerCase().trim().startsWith('http') ? <LiaExternalLinkSquareAltSolid /> : <IoMdDownload /> }
+          </a>
+      }
       </div>
-      <img width="290" height="310" src={ project.image } alt={ project.title } onClick={ setImages } />
+      <img
+        width="290" height="310" src={ project.image } alt={ project.title } onClick={ setImages }
+      />
       <div className="project__footer">
         <p className="description">{ project.description }{ project.languages.length === 0 ? <></> : <> - <span>{ project.languages.join(' / ') }</span></> }</p>
       </div>
--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -4,8 +4,10 @@ import { type ImagesContextValueInterface } from '@app/types-interfaces'
 const defaultImagesContextValue: ImagesContextValueInterface = {
   images: [],
   currentImageIndex: 0,
+  maxHeight: false,
   setCurrentImageIndex: (_index: number) => {},
   setImages: (_images: string[]) => {},
+  setMaxHeight: (_maxHeight: boolean) => {},
 }
 
 export const ImagesContext = createContext<ImagesContextValueInterface>(defaultImagesContextValue)
