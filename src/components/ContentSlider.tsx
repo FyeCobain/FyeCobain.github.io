0403424acd407f4f4ea4191c9e182cbec0303e24
Fye <hereisnirvana@hotmail.com>
2024-01-16 13:05:33 -0600

    ContentSlider updated

--- src/components/ContentSlider.tsx
+++ src/components/ContentSlider.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import type { ContentSliderPropsInterface } from '@app/types-interfaces'
 
 export default function ContentSlider(props: ContentSliderPropsInterface) {
+  let classes: string[] = [ 'content-slider' ]
+  classes = classes.concat(props.className.split(' '))
+
   return (
-    <div className={ ('content-slider ' + props.className).trim() }>
+    <div className={ classes.join(' ') }>
       {
         React.Children.map(props.children, (child: React.ReactNode) =>
         <div className="content-slider__element">
