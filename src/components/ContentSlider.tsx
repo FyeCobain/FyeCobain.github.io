8796d3c34d382210d7c394b993a8028393c5e440
Fye <hereisnirvana@hotmail.com>
2024-01-16 19:45:19 -0600

    Code cleaned

--- src/components/ContentSlider.tsx
+++ src/components/ContentSlider.tsx
@@ -5,17 +5,14 @@ export default function ContentSlider(props: ContentSliderPropsInterface) {
   let phoneCols: string = 'n'
   if (props.phoneCols !== undefined)
     phoneCols = props.phoneCols.toString()
-  console.log('thoneCols', phoneCols)
 
   let tabletCols: string = 'n'
   if (props.tabletCols !== undefined)
     tabletCols = props.tabletCols.toString()
-  console.log('tabletCols', tabletCols)
 
   let laptopCols: string = 'n'
   if (props.laptopCols !== undefined)
     laptopCols = props.laptopCols.toString()
-  console.log('laptopCols', laptopCols)
 
   const classes = (`content-slider--phone-${ phoneCols } content-slider--tablet-${ tabletCols } content-slider--laptop-${ laptopCols } ${ props.className }`).trim()
 
