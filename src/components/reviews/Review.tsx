0dd112d736e4a1801411f1d4701e77ea39db2f7a
Fye <hereisnirvana@hotmail.com>
2024-01-22 21:43:49 -0600

    ContentSlider improved

--- src/components/reviews/Review.tsx
+++ src/components/reviews/Review.tsx
@@ -1,17 +1,28 @@
-import { useContext } from 'react'
+import { type MutableRefObject, useContext, useEffect, useRef } from 'react'
 import { IoStarSharp } from 'react-icons/io5'
+import { ContentSliderContext, ImagesContext } from '@app/contexts'
+import type { ContentSliderContextValueInterface, ImagesContextValueInterface, ReviewInterface } from '@app/types-interfaces'
 import { FREELANCER_PROFILE_URL } from '@app/consts/my.info'
-import { ImagesContext } from '@app/contexts'
-import type { ImagesContextValueInterface, ReviewInterface } from '@app/types-interfaces'
 
 export default function Review({ review }: { review: ReviewInterface }) {
   const imagesState: ImagesContextValueInterface = useContext(ImagesContext)
+  const contentSliderState: ContentSliderContextValueInterface = useContext(ContentSliderContext)
+  const imageRef: MutableRefObject<HTMLImageElement | null> = useRef(null)
   const photo: string = review.reviewerPhoto ?? 'https://www.f-cdn.com/assets/main/en/assets/unknown.png'
+
+  useEffect(() => {
+    contentSliderState.addOnClick({
+      element: imageRef.current,
+      onClick: () => {
+        imagesState.setImages([ photo ])
+      },
+    })
+  }, [])
 
   return (
     <div className="review">
       <div className="review__user">
-        <img src={ photo } alt="User" onClick={ () => imagesState.setImages([ photo ]) } />
+        <img ref={ imageRef } src={ photo } alt="User" />
         <p className="review__user--info"><span>{ review.reviewerName }</span>{ review.reviewerCountry === null ? <></> : <><span>&nbsp;&#9866;&nbsp;</span><span>{ review.reviewerCountry }</span></> }</p>
       </div>
       <div className="review__details">
