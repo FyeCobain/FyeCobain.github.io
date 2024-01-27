a4cb460d8b8ad7754da2879d37080c37202e964b
Fye <hereisnirvana@hotmail.com>
2024-01-26 19:59:59 -0600

    Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import { IoLogoGithub } from 'react-icons/io'
+import { LiaExternalLinkSquareAltSolid } from 'react-icons/lia'
+import { GITHUB_USER } from '@app/consts'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        <a className="footer__icon" href={ `https://github.com/${ GITHUB_USER }` } target="_blank" rel="noreferrer">
+          <IoLogoGithub />
+          <LiaExternalLinkSquareAltSolid />
+        </a>{ GITHUB_USER }
+      </p>
+    </footer>
+  )
+}
