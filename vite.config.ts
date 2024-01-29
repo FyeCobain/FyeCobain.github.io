41c468415d35f0a4605efc91c420d200bc30ccdd
Fye <hereisnirvana@hotmail.com>
2024-01-29 12:09:16 -0600

    Version 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -23,7 +22,7 @@ export default defineConfig({
   esbuild: {
     legalComments: 'none',
   },
-  base: '/vite-deploy-test/',
+  base: '/',
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, './src'),
