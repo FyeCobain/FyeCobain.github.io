afcbfc05a1db1af813522f2eacc89efa2693523b
Fye <hereisnirvana@hotmail.com>
2024-01-30 20:11:56 -0600

    ImagesOverlay.tsx refactoring

--- src/components/ImagesOverlay.tsx
+++ src/components/ImagesOverlay.tsx
@@ -5,88 +5,6 @@ import { GrPrevious } from 'react-icons/gr'
 import { ImagesContext } from '@app/contexts'
 import { onDrag, getClientX } from '@app/functions'
 
-// Removes all the images from the slider
-function removeImages(imagesState: ImagesContextValueInterface, targetElement: Element | null = null) {
-  if (targetElement === null || targetElement.classList.contains('images-overlay__slider') || targetElement.classList.contains('images-overlay__close-button')) {
-    imagesState.setImages([])
-    imagesState.setCurrentImageIndex(0)
-    imagesState.setMaxHeight(false)
-  }
-}
-
-// Returns an array of the slider's img containers
-function getImgsContainers(): HTMLDivElement[] {
-  return Array.from(document.querySelectorAll('.images-overlay__slider > DIV'))
-}
-
-// Returns the current's img index
-function getCurrentImgIndex(imgsContainers: HTMLDivElement[]): number {
-  return imgsContainers.findIndex((imgContainer: HTMLDivElement) =>
-    !imgContainer.classList.contains('prev') && !imgContainer.classList.contains('next'))
-}
-
-// Checks if the 'previous' and the 'next' buttons must be visible
-function checkButtons(currentImageIndex: number, imagesCount: number) {
-  const prevButton: HTMLElement | null = document.querySelector('[class*="images-overlay__prev-button"]')
-  const nextButton: HTMLElement | null = document.querySelector('[class*="images-overlay__next-button"]')
-  if (prevButton === null || nextButton === null) return
-
-  if (currentImageIndex === 0)
-    prevButton.classList.add('hide')
-  else
-    prevButton.classList.remove('hide')
-
-  if (currentImageIndex === imagesCount - 1)
-    nextButton.classList.add('hide')
-  else
-    nextButton.classList.remove('hide')
-}
-
-// Show previous image
-function previousImg(imagesState: ImagesContextValueInterface) {
-  const imgsContainers: HTMLDivElement[] = getImgsContainers()
-  if (imgsContainers.length <= 1) return
-  const currentImageIndex = getCurrentImgIndex(imgsContainers)
-  if (currentImageIndex === 0) return
-
-  imgsContainers[currentImageIndex].classList.add('next')
-  imgsContainers[currentImageIndex - 1].classList.remove('prev')
-  imagesState.setCurrentImageIndex(currentImageIndex - 1)
-  checkButtons(currentImageIndex - 1, imgsContainers.length)
-}
-
-// show next image
-function nextImg(imagesState: ImagesContextValueInterface) {
-  const imgsContainers: HTMLDivElement[] = getImgsContainers()
-  if (imgsContainers.length <= 1) return
-  const currentImageIndex = getCurrentImgIndex(imgsContainers)
-  if (currentImageIndex === imgsContainers.length - 1) return
-
-  imgsContainers[currentImageIndex].classList.add('prev')
-  imgsContainers[currentImageIndex + 1].classList.remove('next')
-  imagesState.setCurrentImageIndex(currentImageIndex + 1)
-  checkButtons(currentImageIndex + 1, imgsContainers.length)
-}
-
-// Performs the drag movement
-function dragImg(event: MouseOrTouchEvent, startEvent: MouseOrTouchEvent) {
-  const imgContainer: HTMLDivElement = (event.target as HTMLImageElement).parentNode as HTMLDivElement
-  imgContainer.style.left = `${ getClientX(event) - getClientX(startEvent) }px`
-}
-
-// Adds or removes the 'cursor-grab' and 'no-transitions' classes
-function addDragClasses(image: HTMLImageElement, add: boolean) {
-  add ? image.classList.add('cursor-grab') : image.classList.remove('cursor-grab')
-  const container: HTMLDivElement = image.parentNode as HTMLDivElement
-  add ? container.classList.add('no-transitions') : container.classList.remove('no-transitions')
-}
-
-// Restores the image original position
-function restoreImgPosition(mouseDownEvent: MouseOrTouchEvent | undefined) {
-  if (mouseDownEvent !== undefined)
-    ((mouseDownEvent.target as HTMLImageElement).parentNode as HTMLDivElement).style.left = ''
-}
-
 export default function ImagesOverlay() {
   const imagesState: ImagesContextValueInterface = useContext(ImagesContext)
   const sliderRef: MutableRefObject<DivNullable> = useRef(null)
@@ -95,6 +13,88 @@ export default function ImagesOverlay() {
   const [ touchStartEvent, setTouchStartEvent ] = useState<React.TouchEvent>()
   const [ touchEndEvent, setTouchEndEvent ] = useState<React.TouchEvent>()
   const [ dragInProgress, setDragInProgress ] = useState<boolean>(false)
+
+  // Removes all the images from the slider
+  function removeImages(imagesState: ImagesContextValueInterface, targetElement: Element | null = null) {
+    if (targetElement === null || targetElement.classList.contains('images-overlay__slider') || targetElement.classList.contains('images-overlay__close-button')) {
+      imagesState.setImages([])
+      imagesState.setCurrentImageIndex(0)
+      imagesState.setMaxHeight(false)
+    }
+  }
+
+  // Returns an array of the slider's img containers
+  function getImgsContainers(): HTMLDivElement[] {
+    return Array.from(document.querySelectorAll('.images-overlay__slider > DIV'))
+  }
+
+  // Returns the current's img index
+  function getCurrentImgIndex(imgsContainers: HTMLDivElement[]): number {
+    return imgsContainers.findIndex((imgContainer: HTMLDivElement) =>
+      !imgContainer.classList.contains('prev') && !imgContainer.classList.contains('next'))
+  }
+
+  // Checks if the 'previous' and the 'next' buttons must be visible
+  function checkButtons(currentImageIndex: number, imagesCount: number) {
+    const prevButton: HTMLElement | null = document.querySelector('[class*="images-overlay__prev-button"]')
+    const nextButton: HTMLElement | null = document.querySelector('[class*="images-overlay__next-button"]')
+    if (prevButton === null || nextButton === null) return
+
+    if (currentImageIndex === 0)
+      prevButton.classList.add('hide')
+    else
+      prevButton.classList.remove('hide')
+
+    if (currentImageIndex === imagesCount - 1)
+      nextButton.classList.add('hide')
+    else
+      nextButton.classList.remove('hide')
+  }
+
+  // Show previous image
+  function previousImg(imagesState: ImagesContextValueInterface) {
+    const imgsContainers: HTMLDivElement[] = getImgsContainers()
+    if (imgsContainers.length <= 1) return
+    const currentImageIndex = getCurrentImgIndex(imgsContainers)
+    if (currentImageIndex === 0) return
+
+    imgsContainers[currentImageIndex].classList.add('next')
+    imgsContainers[currentImageIndex - 1].classList.remove('prev')
+    imagesState.setCurrentImageIndex(currentImageIndex - 1)
+    checkButtons(currentImageIndex - 1, imgsContainers.length)
+  }
+
+  // show next image
+  function nextImg(imagesState: ImagesContextValueInterface) {
+    const imgsContainers: HTMLDivElement[] = getImgsContainers()
+    if (imgsContainers.length <= 1) return
+    const currentImageIndex = getCurrentImgIndex(imgsContainers)
+    if (currentImageIndex === imgsContainers.length - 1) return
+
+    imgsContainers[currentImageIndex].classList.add('prev')
+    imgsContainers[currentImageIndex + 1].classList.remove('next')
+    imagesState.setCurrentImageIndex(currentImageIndex + 1)
+    checkButtons(currentImageIndex + 1, imgsContainers.length)
+  }
+
+  // Performs the drag movement
+  function dragImg(event: MouseOrTouchEvent, startEvent: MouseOrTouchEvent) {
+    const imgContainer: HTMLDivElement = (event.target as HTMLImageElement).parentNode as HTMLDivElement
+    imgContainer.style.left = `${ getClientX(event) - getClientX(startEvent) }px`
+  }
+
+  // Adds or removes the 'cursor-grab' and 'no-transitions' classes
+  function addDragClasses(image: HTMLImageElement, add: boolean) {
+    add ? image.classList.add('cursor-grab') : image.classList.remove('cursor-grab')
+    const container: HTMLDivElement = image.parentNode as HTMLDivElement
+    add ? container.classList.add('no-transitions') : container.classList.remove('no-transitions')
+  }
+
+  // Restores the image original position
+  function restoreImgPosition(mouseDownEvent: MouseOrTouchEvent | undefined) {
+    if (mouseDownEvent !== undefined)
+      ((mouseDownEvent.target as HTMLImageElement).parentNode as HTMLDivElement).style.left = ''
+  }
 
   // Creating the keydown callback
   const onKeyDownCallback = useCallback((event: KeyboardEvent) => {
